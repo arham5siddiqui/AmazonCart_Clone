8e4e6fdfccf5f55c64a1489f5cd014450da8c96e
ArhamS <arham5siddiqui@gmail.com>
2022-01-16 20:02:52 +0530

    initial commit

---
+++ Desktop/React.JS_PROJECTS/my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import CartItems from './components/CartItems';
+import CartTotal from './components/CartTotal';
+
+function App() {
+
+    return ( 
+        <div className = "App" >
+            <Header title={"Amazon Cart"}/ >
+                <div className= "App-main">
+                    <CartItems />
+                    <CartTotal />
+                </div>
+        </div>
+    );
+}
+
+export default App;
