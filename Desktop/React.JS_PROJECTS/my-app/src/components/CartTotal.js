a5c1a7493b81595a91f1aec257aff7c502e1f979
ArhamS <arham5siddiqui@gmail.com>
2022-01-25 18:00:03 +0530

    CHNG: Format Total Price value

--- Desktop/React.JS_PROJECTS/my-app/src/components/CartTotal.js
+++ Desktop/React.JS_PROJECTS/my-app/src/components/CartTotal.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './CartTotal.css'
+import NumberFormat from 'react-number-format';
 
 function CartTotal({items}) {
 
@@ -14,12 +15,13 @@ function CartTotal({items}) {
         })
         return total;
     }
+
     return (
         <div className="CartTotal">
             <h3>
                 Subtotal ({items.length} items):
                 <span className='CartTotal-price'>
-                    ${getTotalPrice()}
+                    <NumberFormat value={getTotalPrice()} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={2} />
                 </span>
             </h3>
             <button>
