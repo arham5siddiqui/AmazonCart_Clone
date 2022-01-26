67e8a4e71edb1b4618b1f344ed5788de31b12340
ArhamS <arham5siddiqui@gmail.com>
2022-01-26 14:03:13 +0530

    ADD: change item Quantity

--- Desktop/React.JS_PROJECTS/my-app/src/components/CartItems.js
+++ Desktop/React.JS_PROJECTS/my-app/src/components/CartItems.js
@@ -3,7 +3,19 @@ import './CartItems.css'
 import CartItem from './CartItem'
 
 
-function CartItems({items}) {
+function CartItems({items, setCartItems}) {
+
+    const changeItemQuantity = (e, index) => {
+        // when we select a quantity on item, we pass it in here
+        // Pass in the index
+        // Using the index we need to change the quantity to the selected one from the selected option
+        // update the items state
+        
+        // **RULE: DO NOT UPDATE THE STATE WITHOUT setter Function (Here the setter function is setCartItems(newItems). )
+        const newItems = [...items];    // creates a CLONE(Deep copy) 
+        items[index].quantity = e.target.value;
+        setCartItems(newItems);    // setter function 
+    }
 
     return (
         <div className="CartItems">
@@ -12,9 +24,10 @@ function CartItems({items}) {
             <div className="CartItems-items">
                 {items.map((item, index) =>
                 <CartItem
-
+                index={index}
                 item={item}
                 key={index}
+                changeItemQuantity={changeItemQuantity}
                 />
                 )}
             </div>
